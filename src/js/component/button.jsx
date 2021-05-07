6538db2c6dadb8b7e06818e89df53810c27e971b
AlbertoCorcheroGarcia <albertocorcherodaw@gmail.com>
2021-05-07 09:21:08 +0000

    "have to change "

--- src/js/component/button.jsx
+++ src/js/component/button.jsx
@@ -3,11 +3,7 @@ import List from "./list.jsx";
     // ya esta en list 
     const [tasksArray, setTasksArray] = useState([]);
 
-
-    const deleteT = indexDelete => {
-        tasksArray.splice(indexDelete, 1);
-    };
-
+    
     // deberia de meterlo en el list ???
     // me falta un puto map seguro al cual le paso el button y el index a borrar
     function onClickDelete (indexDelete){
